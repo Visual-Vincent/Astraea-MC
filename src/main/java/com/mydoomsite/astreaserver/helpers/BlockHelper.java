bf96576ed7ad3cff45b3ab6a33234230d4387405
Visual-Vincent <github-vv@mydoomsite.com>
2021-05-22 18:20:04 +0200

    Made AbstactBlock.use() check in IsInteractable() dynamic

--- src/main/java/com/mydoomsite/astreaserver/helpers/BlockHelper.java
+++ src/main/java/com/mydoomsite/astreaserver/helpers/BlockHelper.java
@@ -7,27 +7,34 @@ import net.minecraft.block.Block;
 
 public final class BlockHelper
 {
-    private static final String USE_METHOD_NAME = "func_225533_a_";
     private static final String USE_METHOD_DEBUG_NAME = "use";
+    private static final String USE_METHOD_COMPILED_NAME = "func_225533_a_";
+    
+    private static final String USE_METHOD_NAME;
     private static final Class<?>[] USE_METHOD_PARAMETER_TYPES;
+    
     
     public static void Init() {};
     
     static
     {
+        String methodName = null;
         Class<?>[] parameterTypes = null;
+        
         for(Method method : AbstractBlock.class.getMethods())
         {
-            if(method.getName() == USE_METHOD_NAME || method.getName() == USE_METHOD_DEBUG_NAME)
+            methodName = method.getName();
+            if(methodName == USE_METHOD_COMPILED_NAME || methodName == USE_METHOD_DEBUG_NAME)
             {
                 parameterTypes = method.getParameterTypes();
                 break;
             }
         }
         
-        if(parameterTypes == null)
-            throw new NullPointerException(String.format("FATAL ERROR: Failed to find method '%s' in %s", USE_METHOD_NAME, AbstractBlock.class.getCanonicalName()));
+        if(methodName == null || parameterTypes == null)
+            throw new NullPointerException(String.format("FATAL ERROR: Failed to find method '%s' in %s", USE_METHOD_DEBUG_NAME, AbstractBlock.class.getCanonicalName()));
         
+        USE_METHOD_NAME = methodName;
         USE_METHOD_PARAMETER_TYPES = parameterTypes;
     }
     
@@ -36,7 +43,7 @@ public final class BlockHelper
         try
         {
             // Checks whether the AbstractBlock.use() method has been overridden
-            return block.getClass().getMethod("use", USE_METHOD_PARAMETER_TYPES).getDeclaringClass().getCanonicalName() != AbstractBlock.class.getCanonicalName();
+            return block.getClass().getMethod(USE_METHOD_NAME, USE_METHOD_PARAMETER_TYPES).getDeclaringClass().getCanonicalName() != AbstractBlock.class.getCanonicalName();
         }
         catch (NoSuchMethodException e)
         {
